05d966e129b87e48739ebf6359713be66b40d5fb
Matheus Romão <matheus_henrique_9@hotmail.com>
2026-07-29 20:13:57 -0300

    inicio do projeto

---
+++ src/main/java/com/hmz/agressores_da_bola/AgressoresDaBolaApplication.java
@@ -0,0 +1,13 @@
+package com.hmz.agressores_da_bola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgressoresDaBolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgressoresDaBolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmz/agressores_da_bola/AgressoresDaBolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmz.agressores_da_bola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgressoresDaBolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
